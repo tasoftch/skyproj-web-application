6d13117cd714f7ac988d7158e418eb390d5c06b6
Thomas Abplanalp <info@tasoft.ch>
2020-04-15 17:25:36 +0200

    Initial Commit

--- SkylineAppData/Classes/Controller/GeneralHTTPErrorController.php
+++ SkylineAppData/Classes/Controller/GeneralHTTPErrorController.php
@@ -5,24 +5,46 @@ namespace Application\Controller;
 
 use Skyline\Application\Controller\AbstractActionController;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class GeneralHTTPErrorController extends AbstractActionController
 {
-    public static function canHandleException(\Throwable $exception): bool {
+	private static $problem;
+
+    public static function canHandleException(Throwable $exception): bool {
         $code = $exception->getCode();
         $methodName = "error{$code}Action";
         if(method_exists(static::class, $methodName)) {
+        	self::$problem = $exception;
             return true;
         }
         return false;
     }
 
+	/**
+	 * @return Throwable
+	 */
+	public static function getProblem()
+	{
+		return self::$problem;
+	}
 
-    public function error404Action(Response $response) {
-        $response->setStatusCode(404);
+	public function errorAction(Response $response, int $code) {
+		$response->setStatusCode($code);
+		$this->renderModel([
+			'PROBLEM' => static::getProblem()
+		]);
 
-        $this->renderTemplate("main", [
-            "Content" => '404'
-        ]);
+		$this->renderTemplate("main", [
+			"Content" => $code
+		]);
+	}
+
+	public function error404Action(Response $response) {
+		$this->errorAction($response, 404);
     }
+
+	public function error403Action(Response $response) {
+		$this->errorAction($response, 403);
+	}
 }
